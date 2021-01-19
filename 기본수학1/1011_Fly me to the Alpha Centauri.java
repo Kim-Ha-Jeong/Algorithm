2f6acecea3d0e28828946e594e9e7522a7f2a63f
Kim-Ha-Jeong <kha031821@gmail.com>
2021-01-20 01:23:30 +0900

    1/19

---
+++ 기본수학1/1011_Fly me to the Alpha Centauri.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+
+public class BOJ_1011 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		int start[] = new int[t];
+		int end[] = new int[t];
+
+		for (int i = 0; i < t; i++) {
+			String s[] = br.readLine().split(" ");
+			start[i] = Integer.parseInt(s[0]);
+			end[i] = Integer.parseInt(s[1]);
+		}
+
+		for (int i = 0; i < t; i++) {
+			int cnt = 0;
+			int distance = end[i] - start[i];
+			int max = (int) Math.sqrt(distance);
+
+			if (max == Math.sqrt(distance))
+				cnt = 2 * max - 1;
+			else if (distance <= max * max + max)
+				cnt = 2 * max;
+			else
+				cnt = 2 * max + 1;
+
+			System.out.println(cnt);
+
+		}
+
+	}
+
+}
---
+++ 기본수학1/10757_큰 수 A+B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.math.*;
+
+public class BOJ_10757 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String AA = sc.next();
+		String BB = sc.next();
+		BigInteger A = new BigInteger(AA);
+		BigInteger B = new BigInteger(BB);
+		
+		System.out.println(A.add(B));
+	}
+
+}

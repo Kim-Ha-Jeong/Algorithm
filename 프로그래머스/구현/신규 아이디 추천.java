02ee57f41c3cdab06f647aaae06dab8c2219e06a
Kim-Ha-Jeong <kha031821@gmail.com>
2021-06-23 21:55:20 +0900

    6/23

---
+++ 프로그래머스/구현/신규 아이디 추천.java
@@ -0,0 +1,27 @@
+class Imple_1 {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+        
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}",".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+        
+        if(answer.equals("")){
+            answer += "a";
+        }
+        
+        if(answer.length() >= 16){
+            answer = answer.substring(0,15);
+            answer = answer.replaceAll("[.]$","");
+        }
+        
+        if(answer.length() <= 2){
+            char last = answer.charAt(answer.length()-1);
+            while(answer.length() < 3){
+                answer += last;
+            }
+        }
+        
+        return answer;
+    }
+}

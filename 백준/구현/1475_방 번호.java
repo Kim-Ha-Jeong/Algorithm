8345651423b0c253bc4c8238f0d3216716cbd3f9
Kim-Ha-Jeong <kha031821@gmail.com>
2021-06-27 22:20:42 +0900

    6/27

---
+++ 백준/구현/1475_방 번호.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+public class Imple_4 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine().trim();
+		char[] num = n.toCharArray();
+		int[] freq = new int[10];
+		int count = 0;
+		
+		for(char a : num) {
+			int partN = Character.getNumericValue(a);
+			freq[partN]++;
+		}
+		
+		int sum = freq[6]+freq[9];
+		
+		if(sum % 2 == 1) {
+			count = Math.max(count, sum/2+1);
+		} else {
+			count = Math.max(count, sum/2);
+		}
+		
+		for(int i=0; i<freq.length; i++) {
+			if(i != 6 && i != 9) {
+				count = Math.max(count, freq[i]);
+			}
+		}
+		
+		System.out.println(count);
+	}
+
+}

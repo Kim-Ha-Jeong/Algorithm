cb72da7f4906c7e60142f37668b3458994a8b7de
Kim-Ha-Jeong <kha031821@gmail.com>
2021-06-30 13:22:35 +0900

    6/30

---
+++ 백준/구현/11656_접미사 배열.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+public class Imple_10 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine().trim();
+		String[] suffix = new String[s.length()];
+		
+		for(int i=0; i<s.length(); i++) {
+			suffix[i] = s.substring(i);
+		}
+		
+		Arrays.sort(suffix);
+		
+		for(int i=0; i<suffix.length; i++) {
+			System.out.println(suffix[i]);
+		}
+	}
+
+}
